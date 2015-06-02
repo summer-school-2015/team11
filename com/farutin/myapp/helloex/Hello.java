85035ee0ba4c39c3b37a7214a53caeef83297de8
VadimFAR <vfarut@mail.ru>
2015-06-02 23:27:03 +0300

    0-HelloWorld

---
+++ com/farutin/myapp/helloex/Hello.java
@@ -0,0 +1,8 @@
+package team11.com.farutin.myapp.helloex;
+//import team11.com.farutin.myapp.helloex.Printer;
+
+class Hello {
+	public static void main(String[] args){
+		team11.com.farutin.myapp.helloex.Printer.class.print();
+	}
+}
---
+++ com/farutin/myapp/helloex/Printer.java
@@ -0,0 +1,7 @@
+package team11.com.farutin.myapp.helloex;
+
+public class Printer {
+	public static void print(){
+		System.out.println("Hello World!");
+	}
+}
