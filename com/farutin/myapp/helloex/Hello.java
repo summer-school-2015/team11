1f877808d993492c78d42d83d6ce55e6a2b381a9
VadimFAR <vfarut@mail.ru>
2015-06-03 11:26:30 +0300

    Delete Hello.java

--- com/farutin/myapp/helloex/Hello.java
+++
@@ -1,8 +0,0 @@
-package team11.com.farutin.myapp.helloex;
-//import team11.com.farutin.myapp.helloex.Printer;
-
-class Hello {
-	public static void main(String[] args){
-		team11.com.farutin.myapp.helloex.Printer.class.print();
-	}
-}
